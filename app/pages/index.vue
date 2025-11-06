3fd0735c28c4c35c7123f927136a318b7fb69e1a
Zydie Morris A. Cansancio <cansanciozydie@gmail.com>
2025-11-06 08:29:03 +0800

    Initial commit

---
+++ app/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Button>Hello</Button>
+    <p class="font-roboto">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias quam
+      tempora consequatur. Excepturi non delectus voluptatibus aperiam at
+      assumenda voluptatum veritatis, sapiente voluptates ex consequatur sint
+      distinctio molestias id neque.
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
